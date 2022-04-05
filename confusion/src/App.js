4b9400b134c8c93b7178588bc47168f3a6773ba9
Ying <lu000097@umn.edu>
2022-04-04 23:59:31 -0500

    components

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap'
 import { Component } from 'react';
+import Menu from './components/MenuComponent';
 
 class App extends Component {
   render() {
     return (
+      
       <div className="App">
         <Navbar dark color="primary">
           <div className="container">
@@ -14,6 +16,7 @@ class App extends Component {
             </NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
